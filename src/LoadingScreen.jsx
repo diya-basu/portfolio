f64452fecebcdcc155945efa5a5e3837609ab1b0
Diya Basu <diya.basu73@gmail.com>
2023-12-13 19:13:58 +0530

    Revert "loading-fix3"
    This reverts commit e64cc654564f89f6365a28c66c3a0d978596a985.

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -19,7 +19,7 @@ const LoadingScreen = () => {
     // Trigger fade-out after 5000 milliseconds (5 seconds)
     const fadeOutTimeout = setTimeout(() => {
       setFade(true);
-    }, 5000);
+    }, 6000);
     // Clear the timeout when the component unmounts or when fade completes
     return () => clearTimeout(fadeOutTimeout);
   }, []);
